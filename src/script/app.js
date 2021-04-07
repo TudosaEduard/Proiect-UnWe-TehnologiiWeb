286ecd5779674ea679fcd6276336f264f6fd6c70
Vlad Rotariu <vladrotariu24@gmail.com>
2021-04-07 18:52:08 +0300

    Identificare județe și afișare în consolă

--- src/script/app.js
+++ src/script/app.js
@@ -1,8 +1,9 @@
+import {countyContainer} from './counties.js'
+
 /* Navbar when scrolling */
 window.onscroll = function(){
     const nav = document.querySelector('.header-wrapper');
     var top = window.scrollY;
-    console.log(top);
 
     if(top >= 1){
         nav.classList.add('active');
@@ -28,6 +29,27 @@ navBtn.addEventListener('click', () => {
         navBtn.classList.remove("fa-window-close");
         navBtn.classList.add("fa-bars");
 
+    }
+})
+
+/* Get the county user clicked on */
+const mapBig = document.querySelector('.map-bg .map');
+mapBig.addEventListener('click', (e) => {
+    let selectedCounty = e.target;
+    for(let i = 0; i < mapBig.children.length; i++) {
+        if (selectedCounty == mapBig.children[i].children[0]){
+            console.log(countyContainer[i]);
+        }
+    }
+})
+
+const mapSmall = document.querySelector('#surface1');
+mapSmall.addEventListener('click', (e) => {
+    let selectedCounty = e.target;
+    for(let i = 0; i < mapSmall.children.length; i++) {
+        if (selectedCounty == mapSmall.children[i].children[0]){
+            console.log(countyContainer[i]);
+        }
     }
 })
 
