4fa3d8e63bef3ef9e4acd2de0fb59759c4a11761
Eduard <tudosa.bogdan88@gmail.com>
2021-04-08 00:54:59 +0300

    select judet page

--- src/script/app.js
+++ src/script/app.js
@@ -31,3 +31,23 @@ navBtn.addEventListener('click', () => {
     }
 })
 
+/* Add and remove items for diagram */
+
+function addItem() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var li = document.createElement("li");
+    
+    li.setAttribute('id', candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
+
+
+function removeItem() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var item = document.getElementById(candidate.value);
+    
+    ul.removeChild(item);
+}
