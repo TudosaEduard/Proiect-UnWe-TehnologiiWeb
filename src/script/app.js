6df4dce65e48f7644c2510e3fd2836791205c731
Vlad Rotariu <vladrotariu24@gmail.com>
2021-04-08 16:17:40 +0300

    implementare mouse hover pe harta de judete

--- src/script/app.js
+++ src/script/app.js
@@ -36,20 +36,46 @@ navBtn.addEventListener('click', () => {
 const mapBig = document.querySelector('.map-bg .map');
 mapBig.addEventListener('click', (e) => {
     let selectedCounty = e.target;
-    for(let i = 0; i < mapBig.children.length; i++) {
-        if (selectedCounty == mapBig.children[i].children[0]){
-            console.log(countyContainer[i]);
-        }
-    }
+    console.log(selectedCounty.getAttribute("name"));
 })
 
 const mapSmall = document.querySelector('#surface1');
 mapSmall.addEventListener('click', (e) => {
-    let selectedCounty = e.target;
-    for(let i = 0; i < mapSmall.children.length; i++) {
-        if (selectedCounty == mapSmall.children[i].children[0]){
-            console.log(countyContainer[i]);
-        }
-    }
+    let selectedCounty = e.target.parentNode;
+    console.log(selectedCounty.getAttribute("name"));
 })
 
+/* Display info when hovering on a county*/
+document.querySelector("#county-details").addEventListener('mousemove', (e) => {
+    let infoBox = document.querySelector("#county-details .info-box");
+    let pageHeight = document.querySelector("#county-details").offsetHeight;
+    let pageWidth = document.querySelector("#county-details").offsetWidth;
+
+    infoBox.style.top = (e.pageY - pageHeight - 30).toString() + "px";
+    infoBox.style.left = (e.pageX - infoBox.offsetWidth / 2).toString() + "px";
+})
+
+const setInfoBoxInfo = (countyName, countyTotalUnemployedNumber) => {
+    console.log(countyName)
+    let countyNameInfo = document.querySelector("#info-box-county-name");
+    let countyTotalUnemployedNumberInfo = document.querySelector("#info-box-county-unemployed-number");
+
+    countyNameInfo.innerHTML = countyName;
+    countyTotalUnemployedNumberInfo.innerHTML = countyTotalUnemployedNumber;
+
+    console.log(countyNameInfo);
+    console.log(countyTotalUnemployedNumberInfo);
+}
+
+document.querySelectorAll("#county-details .map-bg .map a").forEach(elem => {
+    let infoBox = document.querySelector("#county-details .info-box");
+    elem.addEventListener('mouseover', (e) => {
+        let countyName = e.target.getAttribute("name");
+        let countyTotalUnemployedNumber = 1021;
+        setInfoBoxInfo(countyName, countyTotalUnemployedNumber);
+        infoBox.style.visibility = "visible";
+    })
+    elem.addEventListener('mouseleave', (e) => {
+        infoBox.style.visibility = "hidden"; 
+    })
+})
