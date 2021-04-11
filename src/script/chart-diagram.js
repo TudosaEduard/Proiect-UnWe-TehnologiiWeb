5aca94568c1e644c57c1b3c458239f71ae67a158
Eduard <tudosa.bogdan88@gmail.com>
2021-04-11 20:03:43 +0300

    rezolve bug last page

--- src/script/chart-diagram.js
+++ src/script/chart-diagram.js
@@ -1,14 +1,39 @@
 
 var color = ["red" , "green" , "blue" , "yellow" , "orange"];
-var backColor = [];
+var value = [55, 49, 44, 24, 15];
 
-for(var i = 0 ; i < myList.length ; i ++)
+var backColor = [];
+var listValue = [];
+
+for(var i = 0 ; i < myList.length ; i ++){
     backColor.push(color[i]);
+    listValue.push(value[i]);
+}
 
 const line = document.querySelector(".line-chart");
 const bar = document.querySelector(".bar-chart");
 const doughnut = document.querySelector(".doughnut-chart");
 
-line.addEventListener("click" , () => {
-    var myChart = new Chart()
+bar.addEventListener("click" , () => {
+    var xValues = myList;
+    var yValues = listValue;
+    var barColors = backColor;
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Bar-chart"
+    }
+  }
+});
 })
