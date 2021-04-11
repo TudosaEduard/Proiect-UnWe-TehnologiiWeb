2da9fde0e58ef8bc918385d008dd5129db87b629
Eduard <tudosa.bogdan88@gmail.com>
2021-04-11 14:40:39 +0300

    dropdown buttons

---
+++ src/script/chart-diagram.js
@@ -0,0 +1,14 @@
+
+var color = ["red" , "green" , "blue" , "yellow" , "orange"];
+var backColor = [];
+
+for(var i = 0 ; i < myList.length ; i ++)
+    backColor.push(color[i]);
+
+const line = document.querySelector(".line-chart");
+const bar = document.querySelector(".bar-chart");
+const doughnut = document.querySelector(".doughnut-chart");
+
+line.addEventListener("click" , () => {
+    var myChart = new Chart()
+})
