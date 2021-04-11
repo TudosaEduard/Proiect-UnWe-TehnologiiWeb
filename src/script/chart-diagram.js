074153af4bd92830cc8df4460f97729f1b965f82
Vlad Rotariu <vladrotariu24@gmail.com>
2021-04-11 17:22:36 +0300

    implemented chart

--- src/script/chart-diagram.js
+++ src/script/chart-diagram.js
@@ -1,14 +1,102 @@
 
-var color = ["red" , "green" , "blue" , "yellow" , "orange"];
-var backColor = [];
+const linePlot = () => {
+    var xValues = [100,200,300,400,500,600,700,800,900,1000];
 
-for(var i = 0 ; i < myList.length ; i ++)
-    backColor.push(color[i]);
+    new Chart("myChart", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+        borderColor: "red",
+        fill: false
+        },{
+        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+        borderColor: "green",
+        fill: false
+        },{
+        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+        borderColor: "blue",
+        fill: false
+        }]
+    },
+    options: {
+        legend: {display: false},
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontColor: 'white'
+                },
+            }],
+        xAxes: [{
+                ticks: {
+                    fontColor: 'white'
+                },
+            }]
+        } 
+    }
+    });
+}
 
-const line = document.querySelector(".line-chart");
-const bar = document.querySelector(".bar-chart");
-const doughnut = document.querySelector(".doughnut-chart");
+const barPlot = () => {
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = ["red", "green","blue","orange","brown"];
 
-line.addEventListener("click" , () => {
-    var myChart = new Chart()
-})
+    new Chart("myChart", {
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    options: {
+        legend: {display: false},
+        title: {
+        display: true,
+        text: "World Wine Production 2018"
+        }
+    }
+    });
+}
+
+const donughtPlot = () => {
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+    ];
+
+    new Chart("myChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    options: {
+        title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+        }
+    }
+    });
+}
+
+linePlot();
+document.querySelector('#line-chart').addEventListener('mouseon', (e) => {
+    console.log("here");
+});
+
+window.onclick = e => {
+    console.log(e.target);
+}
