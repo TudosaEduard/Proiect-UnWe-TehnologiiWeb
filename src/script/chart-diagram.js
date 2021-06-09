1bc6dd98868f6d9f20a7444997b5f661d6d8dc17
albertandrei29 <albert.onofrei@yahoo.com>
2021-06-09 16:21:28 +0300

    county-statistics page responsive

--- src/script/chart-diagram.js
+++ src/script/chart-diagram.js
@@ -1,152 +1,92 @@
+const barPlot = () => {
+    var xValues = ["under_25 age", "25_to_29 age", "30_to_39 age", "40_to_49 age", "50_to_55 age", "greater_55 age"];
 
-/*let myChart;
+    var ctx = document.getElementById('barChart').getContext('2d');
 
-const initPlot = () => {
-    var xValues = [100,200,300,400,500,600,700,800,900,1000];
+    var yValues = [1000, 2000, 5000, 10000, 20000, 50000];
 
-    myChart = new Chart("myChart", {
-    type: "line",
-    data: {
+    var barColors = ["green", "purple", "blue", "rgb(253, 106, 0)", "rgb(254, 194, 11)", "rgb(64, 176, 166)"];
+  
+    new Chart(ctx, {
+      type: "bar",
+      data: {
         labels: xValues,
         datasets: [{
-        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-        borderColor: "red",
-        fill: false
-        },{
-        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-        borderColor: "green",
-        fill: false
-        },{
-        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-        borderColor: "blue",
-        fill: false
+          label: "Arad", 
+          backgroundColor: barColors,
+          data: yValues
         }]
-    },
-    options: {
-        legend: {display: false},
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    fontColor: 'white'
-                },
-            }],
-        xAxes: [{
-                ticks: {
-                    fontColor: 'white'
-                },
-            }]
-        } 
-    }
+      },
+      options: {
+        /*title: {
+            display: true,
+            text: "Statistica pe varsta"
+        },
+          legend:{
+                display: false,
+                position: 'top',
+          }*/
+        }
+      });
+  }
+
+
+const doughnutPlot = () => {
+
+    var xValues = ["male", "female"];
+
+    var ctx = document.getElementById('doughnutChart').getContext('2d');
+
+    var yValues = [50, 50];
+
+    var doughnutColors = ["red", "blue"];
+  
+    new Chart(ctx, {
+      type: "doughnut",
+      data: {
+        labels: xValues,
+        datasets: [{
+          label: "asda", 
+          backgroundColor: doughnutColors,
+          data: yValues
+        }]
+      },
+      options: {
+        /*title: {
+            display: true,
+            text: "Statistica pe varsta"
+        },
+          legend:{
+                display: false,
+                position: 'top',
+          }*/
+      }
     });
 }
 
 const linePlot = () => {
-    myChart.destroy();
-    var xValues = [100,200,300,400,500,600,700,800,900,1000];
+    var xValues = ["ianuarie", "februarie", "marite"];
 
-    myChart = new Chart("myChart", {
-    type: "line",
-    data: {
+    var ctx = document.getElementById('lineChart').getContext('2d');
+
+    var yValues = [1000, 2000, 5000, 10000, 20000, 50000];
+
+    var lineColors = ["green", "purple", "blue", "red", "black", "white"];
+  
+    new Chart(ctx, {
+      type: "line",
+      data: {
         labels: xValues,
         datasets: [{
-        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-        borderColor: "red",
-        fill: false
-        },{
-        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-        borderColor: "green",
-        fill: false
-        },{
-        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-        borderColor: "blue",
-        fill: false
+          data: [1, 2, 3, 4, 5, 6],
+          fill:false
         }]
-    },
-    options: {
-        legend: {display: false},
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    fontColor: 'white'
-                },
-            }],
-        xAxes: [{
-                ticks: {
-                    fontColor: 'white'
-                },
-            }]
-        } 
-    }
+      },
+      options: {
+      }
     });
 }
 
-/*const barPlot = () => {
-    myChart.destroy();
-    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    var yValues = [55, 49, 44, 24, 15];
-    var barColors = ["red", "green","blue","orange","brown"];
-
-    myChart = new Chart("myChart", {
-    type: "bar",
-    data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-    },
-    options: {
-        legend: {display: false},
-        title: {
-        display: true,
-        text: "World Wine Production 2018"
-        }
-    }
-    });
-}
-
-const donughtPlot = () => {
-    myChart.destroy();
-    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    var yValues = [55, 49, 44, 24, 15];
-    var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
-    ];
-
-    myChart = new Chart("myChart", {
-    type: "doughnut",
-    data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-    },
-    options: {
-        title: {
-        display: true,
-        text: "World Wide Wine Production 2018"
-        }
-    }
-    });
-}
-
-initPlot();
-
-document.querySelector('#line-chart').addEventListener('click', (e) => {
-    linePlot();
-});
-
-document.querySelector('#bar-chart').addEventListener('click', (e) => {
-    barPlot();
-});
-
-document.querySelector('#donught-chart').addEventListener('click', (e) => {
-    donughtPlot();
-});*/
+barPlot();
+doughnutPlot();
+linePlot(); 
