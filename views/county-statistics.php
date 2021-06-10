797f37603363d6c5ee759c286bebb908f8cf5c6a
albertandrei29 <albert.onofrei@yahoo.com>
2021-06-10 04:38:08 +0300

    css included ref

--- views/county-statistics.php
+++ views/county-statistics.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -8,15 +9,16 @@
     <title>Document2</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="src/styles/county-statistics.css">
+    <link rel="stylesheet" href="src/styles/county-statistics-responsive.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
-    
+
 </head>
 
 <body>
 
     <section id="county-statistics">
-    <h1 class="anim" data-animtype="from-top" data-animdelay="0s">Vezi aici statistici de comparare</h1>
+        <h1 class="anim" data-animtype="from-top" data-animdelay="0s">Vezi aici statistici de comparare</h1>
         <div class="charts">
             <div class="bar-chart">
                 <div class="text-bar">
@@ -26,27 +28,29 @@
                     <h1>Alege luna si anul pentru comparare: </h1>
                     <select class="month-bar" id="monthBar">
                         <option value="1">Ianuarie</option>
-                        <option value="2">Februarie</option>   
-                        <option value="3">Martie</option>   
-                        <option value="4">Aprilie</option>   
-                        <option value="5">Mai</option>        
-                        <option value="6">Iunie</option>   
-                        <option value="7">Iulie</option>   
-                        <option value="8">August</option>   
-                        <option value="9">Septembrie</option>   
+                        <option value="2">Februarie</option>
+                        <option value="3">Martie</option>
+                        <option value="4">Aprilie</option>
+                        <option value="5">Mai</option>
+                        <option value="6">Iunie</option>
+                        <option value="7">Iulie</option>
+                        <option value="8">August</option>
+                        <option value="9">Septembrie</option>
                         <option value="10">Octombrie</option>
-                        <option value="11">Noiembrie</option>  
-                        <option value="12">Decembrie</option>     
-                     </select>
-                     <select class="year-bar" id="yearBar">
+                        <option value="11">Noiembrie</option>
+                        <option value="12">Decembrie</option>
+                    </select>
+                    <select class="year-bar" id="yearBar">
                         <option value="2021">2021</option>
-                        <option value="2020">2020</option>     
-                     </select>
-                </div>  
+                        <option value="2020">2020</option>
+                    </select>
+                </div>
                 <canvas class="chart-canvas" id="barChart"></canvas>
                 <div class="buttons-bar">
-                    <button id="download-chart-bar" class="download-button"><i class="fa fa-download"></i>  Descarca grafic</button>
-                    <button id="download-data-bar" class="download-button"><i class="fa fa-download"></i>  Descarca datele</button>
+                    <button id="download-chart-bar" class="download-button"><i class="fa fa-download"></i> Descarca
+                        grafic</button>
+                    <button id="download-data-bar" class="download-button"><i class="fa fa-download"></i> Descarca
+                        datele</button>
                 </div>
             </div>
             <div class="doughnut-chart">
@@ -57,27 +61,29 @@
                     <h1>Alege luna si anul pentru comparare: </h1>
                     <select class="month-doughnut" id="monthDoughnut">
                         <option value="1">Ianuarie</option>
-                        <option value="2">Februarie</option>   
-                        <option value="3">Martie</option>   
-                        <option value="4">Aprilie</option>   
-                        <option value="5">Mai</option>        
-                        <option value="6">Iunie</option>   
-                        <option value="7">Iulie</option>   
-                        <option value="8">August</option>   
-                        <option value="9">Septembrie</option>   
+                        <option value="2">Februarie</option>
+                        <option value="3">Martie</option>
+                        <option value="4">Aprilie</option>
+                        <option value="5">Mai</option>
+                        <option value="6">Iunie</option>
+                        <option value="7">Iulie</option>
+                        <option value="8">August</option>
+                        <option value="9">Septembrie</option>
                         <option value="10">Octombrie</option>
-                        <option value="11">Noiembrie</option>  
-                        <option value="12">Decembrie</option>     
+                        <option value="11">Noiembrie</option>
+                        <option value="12">Decembrie</option>
                     </select>
                     <select class="year-doughnut" id="yearDoughnut">
                         <option value="2021">2021</option>
-                        <option value="2020">2020</option>    
-                     </select>
+                        <option value="2020">2020</option>
+                    </select>
                 </div>
                 <canvas class="chart-canvas" id="doughnutChart"></canvas>
                 <div class="buttons-doughnut">
-                    <button id="download-chart-doughnut" class="download-button"><i class="fa fa-download"></i>  Descarca grafic</button>
-                    <button id="download-data-doughnut" class="download-button"><i class="fa fa-download"></i>  Descarca datele</button>
+                    <button id="download-chart-doughnut" class="download-button"><i class="fa fa-download"></i> Descarca
+                        grafic</button>
+                    <button id="download-data-doughnut" class="download-button"><i class="fa fa-download"></i> Descarca
+                        datele</button>
                 </div>
                 <canvas class="chart-canvas" id="femaleChart"></canvas>
             </div>
@@ -89,46 +95,48 @@
                     <h1>Alege intervalul de timp: </h1>
                     <select class="month-from" id="monthFrom">
                         <option value="1">Ianuarie</option>
-                        <option value="2">Februarie</option>   
-                        <option value="3">Martie</option>   
-                        <option value="4">Aprilie</option>   
-                        <option value="5">Mai</option>        
-                        <option value="6">Iunie</option>   
-                        <option value="7">Iulie</option>   
-                        <option value="8">August</option>   
-                        <option value="9">Septembrie</option>   
+                        <option value="2">Februarie</option>
+                        <option value="3">Martie</option>
+                        <option value="4">Aprilie</option>
+                        <option value="5">Mai</option>
+                        <option value="6">Iunie</option>
+                        <option value="7">Iulie</option>
+                        <option value="8">August</option>
+                        <option value="9">Septembrie</option>
                         <option value="10">Octombrie</option>
-                        <option value="11">Noiembrie</option>  
-                        <option value="12">Decembrie</option>         
+                        <option value="11">Noiembrie</option>
+                        <option value="12">Decembrie</option>
                     </select>
                     <select class="year-from" id="yearFrom">
                         <option value="2021">2021</option>
-                        <option value="2020">2020</option>     
-                     </select>
-                     <p class="line">-</p>
+                        <option value="2020">2020</option>
+                    </select>
+                    <p class="line">-</p>
                     <select class="month-to" id="monthTo">
                         <option value="1">Ianuarie</option>
-                        <option value="2">Februarie</option>   
-                        <option value="3">Martie</option>   
-                        <option value="4">Aprilie</option>   
-                        <option value="5">Mai</option>        
-                        <option value="6">Iunie</option>   
-                        <option value="7">Iulie</option>   
-                        <option value="8">August</option>   
-                        <option value="9">Septembrie</option>   
+                        <option value="2">Februarie</option>
+                        <option value="3">Martie</option>
+                        <option value="4">Aprilie</option>
+                        <option value="5">Mai</option>
+                        <option value="6">Iunie</option>
+                        <option value="7">Iulie</option>
+                        <option value="8">August</option>
+                        <option value="9">Septembrie</option>
                         <option value="10">Octombrie</option>
-                        <option value="11">Noiembrie</option>  
-                        <option value="12">Decembrie</option>          
-                     </select>
-                     <select class="year-to" id="yearTo">
+                        <option value="11">Noiembrie</option>
+                        <option value="12">Decembrie</option>
+                    </select>
+                    <select class="year-to" id="yearTo">
                         <option value="2021">2021</option>
-                        <option value="2020">2020</option>     
-                     </select>
-                </div>  
+                        <option value="2020">2020</option>
+                    </select>
+                </div>
                 <canvas class="chart-canvas" id="lineChart"></canvas>
                 <div class="buttons-line">
-                    <button id="download-chart-line" class="download-button"><i class="fa fa-download"></i>  Descarca grafic</button>
-                    <button id="download-data-line" class="download-button"><i class="fa fa-download"></i>  Descarca datele</button>
+                    <button id="download-chart-line" class="download-button"><i class="fa fa-download"></i> Descarca
+                        grafic</button>
+                    <button id="download-data-line" class="download-button"><i class="fa fa-download"></i> Descarca
+                        datele</button>
                 </div>
             </div>
         </div>
